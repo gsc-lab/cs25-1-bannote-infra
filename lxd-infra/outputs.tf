8938da57c08d5912771092f26e654dc992b34ef9
kyumin1227 <kyumin12271227@gmail.com>
2025-09-24 16:56:47 +0900

    fix: k3s 설치 후 아이피가 추가 되어 앤서블 연결 아이피를 v6로 변경

--- lxd-infra/outputs.tf
+++ lxd-infra/outputs.tf
@@ -1,14 +1,18 @@
 output "instance_ips" {
   description = "The IP addresses of the created LXD instances"
   value = {
-    for k, v in lxd_instance.cluster_vms : k => v.ipv4_address
+    for k, v in lxd_instance.cluster_vms : k => one([
+      for ip in v.network[0].addresses : ip.address if ip.family == "inet6" and ip.scope == "global"
+    ])
   }
   sensitive = true
 }
 
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/templates/inventory.tftpl", {
-    instance_ips = { for k, v in lxd_instance.cluster_vms : k => v.ipv4_address }
+    instance_ips = { for k, v in lxd_instance.cluster_vms : k => one([
+      for ip in v.network[0].addresses : ip.address if ip.family == "inet6" and ip.scope == "global"
+    ]) }
     env = var.environment
   })
   filename = "${pathexpand("~")}/inventory/inventory-${var.environment}"
