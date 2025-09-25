33488058bb2c5b5c0756cabd81ae6571e8041ddc
kyumin1227 <kyumin12271227@gmail.com>
2025-09-25 14:21:37 +0900

    fix: 외부 연결 ip4만 추출하여 inventory로 저장
    - show terraform output 별도 출력 제거
    - playbook 양식 변경에 따른 수정 yml -> yaml

--- lxd-infra/outputs.tf
+++ lxd-infra/outputs.tf
@@ -1,24 +1,34 @@
-output "instance_ips" {
-  description = "The IP addresses of the created LXD instances"
-  value = {
-    for k, v in lxd_instance.cluster_vms : k => one([
-      for ip in v.network[0].addresses : ip.address if ip.family == "inet6" and ip.scope == "global"
-    ])
+# lxd의 외부 연결 ip4 아이피를 추출
+locals {
+  public_ipv4_addresses = {
+    for k, v in lxd_instance.cluster_vms : k => element(
+      flatten([
+        for iface_data in values(v.interfaces) : [
+          for ip in iface_data.ips : ip.address if ip.family == "inet" && ip.scope == "global"
+        ]
+      ]),
+      0
+    )
   }
-  sensitive = true
 }
 
+# 외부 연결 ip4 출력
+output "public_ipv4_addresses" {
+  description = "The public IPv4 addresses of the instances, dynamically filtered from all interfaces."
+  value       = local.public_ipv4_addresses
+}
+
+# 앤서블에서 이용 할 수 있도록, ~/inventory/inventory-{env}에 저장
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/templates/inventory.tftpl", {
-    instance_ips = { for k, v in lxd_instance.cluster_vms : k => one([
-      for ip in v.network[0].addresses : ip.address if ip.family == "inet6" and ip.scope == "global"
-    ]) }
+    instance_ips = local.public_ipv4_addresses
     env = var.environment
   })
   filename = "${pathexpand("~")}/inventory/inventory-${var.environment}"
   file_permission = "0644"
 }
 
+# 저장된 파일 주소 출력
 output "inventory_file" {
   description = "Path to the generated Ansible inventory file"
   value = local_file.ansible_inventory.filename
